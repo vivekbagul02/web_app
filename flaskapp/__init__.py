247b24245f46728dd945deab07535336fb3cabf1
vivekbagul02 <129834079+vivekbagul02@users.noreply.github.com>
2025-04-20 20:35:16 +0530

    Update __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -15,11 +15,12 @@ app.config.from_mapping(SECRET_KEY=os.environ['SECRET_KEY'])
 
 @app.route('/')
 def index():
-    instance_id = 'Unknown'
-    try:
-        response = requests.get('http://169.254.169.254/latest/meta-data/instance-id/', timeout=3)
-        instance_id = response.content.decode('utf-8')
-    except:
-        pass
+    return "hello world"
+    # instance_id = 'Unknown'
+    # try:
+    #     response = requests.get('http://169.254.169.254/latest/meta-data/instance-id/', timeout=3)
+    #     instance_id = response.content.decode('utf-8')
+    # except:
+    #     pass
 
-    return render_template('index.html', instance_id=instance_id)
+    # return render_template('index.html', instance_id=instance_id)
