798d15e262b62cfd88a5f8d050706e45cf5bffd1
vivekbagul02 <129834079+vivekbagul02@users.noreply.github.com>
2025-04-21 00:57:41 +0530

    Update userdata.sh

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -8,8 +8,7 @@ yum install gcc -y
 yum install build-essential -y
 yum install python3-pip python3-devel python3-setuptools -y
 
-git config --system credential.https://git-codecommit.us-east-1.amazonaws.com.helper '!aws --profile default codecommit credential-helper $@'
-git config --system credential.https://git-codecommit.us-east-1.amazonaws.com.UseHttpPath true
+
 
 aws configure set region us-east-1
 
@@ -23,7 +22,7 @@ cd /var/www
 git config core.fileMode false
 
 # update me
-aws s3 cp s3://tci-s3-demo/simple-flask/.env .env
+aws s3 cp s3://tci-s3-bucket/simple-flask/.env .env
 
 chmod +x scripts/post_userdata.sh
 
